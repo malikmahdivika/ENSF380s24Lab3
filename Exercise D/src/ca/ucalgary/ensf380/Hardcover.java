4381e199c4c57c2afd62eb43ed85b4b700f59598
Soummadip Sarkar <144164458+Official-Soumik06@users.noreply.github.com>
2024-07-18 23:01:29 -0600

    Finished Implementations For Hardcover.java Class

--- Exercise D/src/ca/ucalgary/ensf380/Hardcover.java
+++ Exercise D/src/ca/ucalgary/ensf380/Hardcover.java
@@ -2,4 +2,10 @@ package ca.ucalgary.ensf380;
 
 public class Hardcover {
 
+	//Public Other Member Methods/Functions: 
+	
+	public String binding() {
+        return "The Method 'binding' Is Called From The Hardcover Class";
+    }
+	
 }
