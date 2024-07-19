7fbb8da55c8406040ea5548afa017784ce358dbe
Soummadip Sarkar <144164458+Official-Soumik06@users.noreply.github.com>
2024-07-18 23:05:04 -0600

    Finished Implementations For Paperback.java Class

--- Exercise D/src/ca/ucalgary/ensf380/Paperback.java
+++ Exercise D/src/ca/ucalgary/ensf380/Paperback.java
@@ -1,5 +1,11 @@
 package ca.ucalgary.ensf380;
 
 public class Paperback {
-
+	
+	//Public Other Member Methods/Functions:
+	
+	 public String coverArt() {
+	        return "The Method 'coverArt' Is Called From The Paperback Class";
+	    }
+	
 }
