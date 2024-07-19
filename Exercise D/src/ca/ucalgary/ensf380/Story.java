6b01e4f8805a3e4023159afd692b43df2f778611
Soummadip Sarkar <144164458+Official-Soumik06@users.noreply.github.com>
2024-07-18 23:07:15 -0600

    Finished Implementations For Story.java Class

--- Exercise D/src/ca/ucalgary/ensf380/Story.java
+++ Exercise D/src/ca/ucalgary/ensf380/Story.java
@@ -2,4 +2,25 @@ package ca.ucalgary.ensf380;
 
 public class Story {
 
+	//Private Data Members:
+	
+	private Author[] theAuthors;
+	
+	//Public Getter Member Methods/Functions: 
+	
+	public Author[] getTheAuthors() {
+		return theAuthors;
+	}
+	
+	//Public Setter Member Methods/Functions:
+	
+	public void setTheAuthors(Author[] theAuthors) {
+		this.theAuthors = theAuthors;
+	}
+	
+	//Public Other Member Methods/Functions:
+	
+	public String plot() {
+        return "The Method 'plot' Is Called From The Story Class";
+    }
 }
