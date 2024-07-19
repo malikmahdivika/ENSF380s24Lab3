781a49d3cb516acdfe16dbbc1be8a4809988e64d
Soummadip Sarkar <144164458+Official-Soumik06@users.noreply.github.com>
2024-07-18 23:04:23 -0600

    Finished Implementations For Novel.java Class

--- Exercise D/src/ca/ucalgary/ensf380/Novel.java
+++ Exercise D/src/ca/ucalgary/ensf380/Novel.java
@@ -2,4 +2,34 @@ package ca.ucalgary.ensf380;
 
 public class Novel {
 
+	//Private Data Members: 
+	
+	private Author[] theAuthors;
+	private Series mySeries;
+	
+	//Public Getter Member Methods/Functions: 
+	
+	public Author[] getTheAuthors() {
+		return theAuthors;
+	}
+	
+	public Series getMySeries() {
+		return mySeries;
+	}
+	
+	//Public Setter Member Methods/Functions:
+	
+	public void setTheAuthors(Author[] theAuthors) {
+		this.theAuthors = theAuthors;
+	}
+	
+	public void setMySeries(Series mySeries) {
+		this.mySeries = mySeries;
+	}
+	
+	//Public Other Member Methods/Functions: 
+	
+	public String theme() {
+        return "The Method 'theme' Is Called From The Novel Class";
+    }
 }
