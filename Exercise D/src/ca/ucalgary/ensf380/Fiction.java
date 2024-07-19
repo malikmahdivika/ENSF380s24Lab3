1f8db4836c9eba5cf575300dafa597d2f347fb68
Soummadip Sarkar <144164458+Official-Soumik06@users.noreply.github.com>
2024-07-18 23:00:43 -0600

    Finished Implementations For Fiction.java Class

--- Exercise D/src/ca/ucalgary/ensf380/Fiction.java
+++ Exercise D/src/ca/ucalgary/ensf380/Fiction.java
@@ -2,4 +2,13 @@ package ca.ucalgary.ensf380;
 
 public class Fiction {
 
+	//Public Other Member Methods/Functions:
+	
+	public String coverArt() {
+        return "The Method 'coverArt' Is Called From The Fiction Class";
+    }
+	
+	public String genre() {
+        return "The Method 'genre' Is Called From The Fiction Class";
+    }
 }
