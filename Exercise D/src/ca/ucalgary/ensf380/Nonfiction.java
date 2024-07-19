45a8e059599d879b0ba9bad74d0846ec79cca9e8
Soummadip Sarkar <144164458+Official-Soumik06@users.noreply.github.com>
2024-07-18 23:03:36 -0600

    Finished Implementations For Nonfiction.java Class

--- Exercise D/src/ca/ucalgary/ensf380/Nonfiction.java
+++ Exercise D/src/ca/ucalgary/ensf380/Nonfiction.java
@@ -2,4 +2,25 @@ package ca.ucalgary.ensf380;
 
 public class Nonfiction {
 
+	//Private Data Members:
+	
+	private Category deweyClassification;
+	
+	//Public Getter Member Methods/Functions: 
+	
+	public Category getDeweyClassification() {
+		return deweyClassification;
+	}
+	
+	//Public Setter Member Methods/Functions: 
+	
+	public void setDeweyClassification(Category deweyClassification) {
+		this.deweyClassification = deweyClassification;
+	}
+	
+	//Public Other Member Methods/Functions: 
+	
+	public String topic() {
+        return "The Method 'topic' Is Called From The Nonfiction Class";
+    }
 }
