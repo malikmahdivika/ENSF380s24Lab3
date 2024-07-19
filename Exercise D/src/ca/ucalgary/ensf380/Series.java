a270808f5933c46863c3b1956feda37fa925233f
Soummadip Sarkar <144164458+Official-Soumik06@users.noreply.github.com>
2024-07-18 23:06:33 -0600

    Finished Implementations For Series.java Class

--- Exercise D/src/ca/ucalgary/ensf380/Series.java
+++ Exercise D/src/ca/ucalgary/ensf380/Series.java
@@ -1,5 +1,26 @@
 package ca.ucalgary.ensf380;
 
 public class Series {
-
+	
+	//Private Data Members:
+	
+	private String seriesName;
+	
+	//Public Getter Member Methods/Functions:
+	
+	public String getSeriesName() {
+		return seriesName;
+	}
+	
+	//Public Setter Member Methods/Functions:
+	
+	public void setSeriesName(String seriesName) {
+		this.seriesName = seriesName;
+	}
+	
+	//Public Other Member Methods/Functions:
+	
+	public String theme() {
+        return "The Method 'theme' Is Called From The Series Class";
+    }
 }
