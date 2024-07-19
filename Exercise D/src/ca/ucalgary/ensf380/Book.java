414f851405c3f0967537831049a360e0cb3060f2
Soummadip Sarkar <144164458+Official-Soumik06@users.noreply.github.com>
2024-07-18 22:57:33 -0600

    Finished Implementations For Book.java Class

--- Exercise D/src/ca/ucalgary/ensf380/Book.java
+++ Exercise D/src/ca/ucalgary/ensf380/Book.java
@@ -2,4 +2,53 @@ package ca.ucalgary.ensf380;
 
 public class Book {
 
+	//Private Data Members:
+	
+	private String isbn;
+	private int publicationYear;
+	private int pages;
+	
+	//Public Default Constructor
+	
+	public Book() {
+		this.isbn = "Unknown";
+		this.publicationYear = 0;
+		this.pages = 0;
+	}
+	
+	//Public Other Constructor:
+	
+	public Book(String isbn, int publicationYear, int pages) {
+		this.isbn = isbn;
+		this.publicationYear = publicationYear;
+		this.pages = pages;
+	}
+	
+	//Public Getter Member Methods/Functions:
+	
+	public String getIsbn() {
+		return isbn;
+	}
+	
+	public int getPublicationYear() {
+		return publicationYear;
+	}
+	
+	public int getPages() {
+		return pages;
+	}
+	
+	//Public Setter Member Methods/Functions:
+	
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	
+	public void setPublicationYear(int publicationYear) {
+		this.publicationYear = publicationYear;
+	}
+	
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
 }
