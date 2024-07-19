62910e6010e06f1ad3dfd529ee7e7e916f36f5c2
Soummadip Sarkar <144164458+Official-Soumik06@users.noreply.github.com>
2024-07-18 22:55:26 -0600

    Finished Implementations For Anthology.java File

--- Exercise D/src/ca/ucalgary/ensf380/Anthology.java
+++ Exercise D/src/ca/ucalgary/ensf380/Anthology.java
@@ -2,4 +2,35 @@ package ca.ucalgary.ensf380;
 
 public class Anthology {
 
+	//Private Data Members:
+	
+	private Story[] stories;
+	
+	//Public Getter Member Methods/Functions:
+	
+	public Story[] getStory() {
+		return stories;
+	}
+	
+	//Public Setter Member Methods/Functions:
+	
+	public void setStory(Story[] stories) {
+		this.stories = stories;
+	}
+
+	//Other Member Methods/Functions:
+	
+	public String storyOrder() {
+        if (stories == null || stories.length == 0) {
+            return "No Stories Are Available";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < stories.length; i++) {
+            sb.append("Story ").append(i + 1).append(": ").append(stories[i].toString()).append("\n");
+        }
+
+        return sb.toString();
+    }
 }
+
